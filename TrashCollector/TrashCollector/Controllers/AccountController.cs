65895f680d4b3e254aaefbf5a1a56a09ba595b99
unknown <kylee.schreibs2@yahoo.com>
2019-05-04 23:58:02 -0500

    add user conditionals to AccountController

--- TrashCollector/TrashCollector/Controllers/AccountController.cs
+++ TrashCollector/TrashCollector/Controllers/AccountController.cs
@@ -156,22 +156,34 @@ namespace TrashCollector.Controllers
                 // if customer else if employee
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
-            }
+                    await UserManager.AddToRoleAsync(user.Id, model.UserRoles);
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
+                    if (model.UserRoles == "Employee")
+                    {
+                        return RedirectToAction("Create", "EmployeeController");
+                    }
+                    else if (model.UserRoles == "Customer")
+                    {
+                        return RedirectToAction("Create", "CustomerController");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                // If we got this far, something failed, redisplay form
+                return View(model);
+            }
         }
 
         //
